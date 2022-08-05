10552c98ef971f5d7b4ada465aa77893a6470b09
ERNANI DA PAZ <ernani.2990@aluno.pr.senac.br>
2022-08-04 22:41:03 -0300

    criando leitura do .env

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo "hello good";
+
+function dd($input){ //danf no codigo,só definindo, mostra valor da variavel//
+    var_dump($input);// mostra todas as informacoes do projeto, ixibir,mostrar//
+    die(); //tipo um break//
+}
+
+$mainPosition = __DIR__;
+
+require_once("{$mainPosition}\bootstrap\Env.php");// tudo que vai carregar, comecar,levantar, estartar, primeiras camadas//
+// .env 
+
+$env = new Env();
+dd($env);
