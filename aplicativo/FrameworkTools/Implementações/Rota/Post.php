7215a34e7898a3e031974ac26679ab93e09a9de1
ernani422 <110068726+ernani422@users.noreply.github.com>
2022-10-23 00:27:57 -0300

    atualizando

--- aplicativo/FrameworkTools/Implementações/Rota/Post.php
+++ aplicativo/FrameworkTools/Implementações/Rota/Post.php
@@ -1,4 +1,6 @@
 <?php
+/* Esse arquivo está correto, porém esta no caminho e pasta errada, pfv retorne clicando no framework-senac02.2022 
+e verifique as pastas abaixo que está com os caminhos corretos*/
 
 namespace App\FrameworkTools\Implementations\Route;
 
