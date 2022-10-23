e0669d156eb083f8e1a33185a456d6304b8e7d4f
ernani422 <110068726+ernani422@users.noreply.github.com>
2022-10-23 00:14:42 -0300

    atualização

---
+++ app/FrameworkTools/Implementations/Route/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\FrameworkTools\Implementations\Route;
+
+use App\Controllers\InsertCarController;
+use App\Controllers\InsertDataController;
+
+
+trait Post {
+    
+    private static function post() {
+        switch (self::$processServerElements->getRoute()) {
+                    
+            case '/carro-insert':
+                return (new InsertCarController)->exec();
+            break;
+            case '/insert-data':
+                return (new InsertDataController)->exec();  
+            break;
+                case '/carro-insert':
+                return (new InsertCarController)->exec();
+            break;
+
+
+        }
+    }
+
+}
